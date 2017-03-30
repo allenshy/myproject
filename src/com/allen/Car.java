fc4a31dbc56f6784388893b89ab5015d969c6fc8
Apple <Apple@192.168.2.1>
2017-03-30 17:16:23 +0800

    啦啦啦

--- src/com/allen/Car.java
+++ src/com/allen/Car.java
@@ -3,8 +3,7 @@ package com.allen;
 public class Car {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
--- src/com/allen/CarTester.java
+++ src/com/allen/CarTester.java
@@ -3,8 +3,12 @@ package com.allen;
 public class CarTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	
+	
+	
+	
+	
 	}
 
 }
