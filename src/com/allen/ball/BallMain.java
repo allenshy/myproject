1284d19f79dfbe9dc4c5720c5cb53b1b936922b8
allen <allen@192.168.0.12>
2018-06-09 12:10:33 +0800

    題目

---
+++ src/com/allen/ball/BallMain.java
@@ -0,0 +1,20 @@
+/*讀取bingo.txt，資料如下:
+ * 25
+ * 8,4,7,18,21,3,5,1,9,10
+ * 
+ * 有5X共25顆球，亂數排列印出
+ */
+
+
+
+
+package com.allen.ball;
+
+public class BallMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/gogogame/GoGoMain.java
@@ -0,0 +1,19 @@
+/*有一遊戲場地，長寬由執行當時決定，場地中亂數產生陷阱，陷阱數也由當時決定
+ * 
+ * 玩家初始HP為100，走一步消耗1，撞邊界扣5，踩陷阱扣20
+ * 
+ * 有個檔案(maze.txt)資訊如下:
+ * 6,4
+ * 5
+ * 6,8,2,4,2,2,6,6,8,4,2,2,6
+ */
+package gogogame;
+
+public class GoGoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
